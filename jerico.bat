98ee0c3ba7b3710e961aa56c1975cabbf0c7cb3a
cit142f-12@M307.UC-BCF.EDU.PH <cit142f-12@M307.UC-BCF.EDU.PH>
2022-09-14 17:36:38 +0800

    first upload

---
+++ jerico.bat
@@ -0,0 +1,7 @@
+@echo off
+git remote set-url origin https://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@github.com/Brenthjerico/cit14brenth.git
+pause
+git add . 
+git commit -m "first upload"
+git push
+pause
